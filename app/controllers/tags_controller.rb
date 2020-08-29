31393319445fa6383333cae243006dcf1e08a093
Shannon Ryan <shannon.ryan@envato.com>
2020-08-29 12:17:55 +1000

    Style buttons

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tags = Tag.all
+    @tags = Tag.all.order(:name)
   end
 
   def new
