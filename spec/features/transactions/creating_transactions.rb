36ada063ccd1320373bd3cd84cda47d51216de54
sjryan74 <shanno.ryan@gmail.com>
2020-09-01 17:34:04 +1000

    Add tag names to form

--- spec/features/transactions/creating_transactions.rb
+++ spec/features/transactions/creating_transactions.rb
@@ -15,4 +15,22 @@ RSpec.feature "Create transactions" do
     expect(page).to have_content("Netflix payment")
     expect(page).to have_content("20.60")
   end
+
+  scenario "with tags" do
+    visit "/transactions"
+
+    click_link "New Transaction"
+
+    fill_in "Trans Date", with: Time.now
+    fill_in "Amount", with: 20.6
+    fill_in "Description", with: "Bill payment"
+    fill_in "Tags", with: "utility,gas"
+    click_button "Create Transaction"
+
+    expect(page).to have_content("Transaction created")  
+    expect(page).to have_content("Bill payment")
+    expect(page).to have_content("20.60")
+    expect(page).to have_content("gas")
+    expect(page).to have_content("utility")
+  end
 end
